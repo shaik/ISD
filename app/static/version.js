da93308cb9b8d8b489b4b9dfff99a18f0da81415
Shai Kfir <shai.kfir@gmail.com>
2025-03-29 18:40:32 +0300

    Bump version to 1.1.2

--- app/static/version.js
+++ app/static/version.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "1.1.1";
-const BUILD_NUMBER = "2024.03.28.4";
+const APP_VERSION = "1.1.2";
+const BUILD_NUMBER = "2024.03.28.5";
 const APP_VERSION_OBJ = {
     version: APP_VERSION,
     build: BUILD_NUMBER,
